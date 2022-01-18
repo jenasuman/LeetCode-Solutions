d21068e5ad2a66c334e43159a1e26c660fb063e1
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-01-18 10:00:16 +0530

    Time: 38 ms (8.75%), Space: 20.4 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -2,49 +2,24 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int m) {
         
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        
         int n=flowerbed.size();
         
-        int countbegin=0;
-        
-        int i=0;
-        
-        while(i<n && flowerbed[i]==0){
-            countbegin++;
-            i++;
-        }
-        
-        if(countbegin==1 && n==1){
-            return true;
-        }
-        
-        int count=0;
-        
-        if(i!=n)
-        count+=(countbegin>0)?ceil((countbegin-1)/2.0):0;
-        else{
-            count+=ceil((countbegin)/2.0);
-        }
-        
-        
-        
-        int countMiddle=0;
-        
-        for(;i<n;i++){
+        for(int i=1;i<n-1;i++){
             
-            if(flowerbed[i]==1){
-                count+=(countMiddle>0)?ceil((countMiddle-2)/2.0):0;
-                countMiddle=0;
-            }
-            else{
-                countMiddle++;
+            if(flowerbed[i]+flowerbed[i-1]+flowerbed[i+1]==0){
+                --m;
+                i++;
             }
             
             
+            
+            
         }
+        if(m<=0)return true;
         
-             count+=(countMiddle>0)?ceil((countMiddle-1)/2.0):0;
-        
-         
-        return count>=m;
+        return false;
     }
 };
