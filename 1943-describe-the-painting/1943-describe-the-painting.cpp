dd2905d6b485cf0d0cc4c742f766509d039bbc75
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-02-04 00:14:19 +0530

    Time: 747 ms (28.30%), Space: 109.2 MB (31.45%) - LeetHub

---
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
+        
+        vector<vector<long long>> ans;
+        
+        map<long long,long long> m;
+        
+       
+        for(auto v:segments){
+            m[v[0]]+=v[2];
+            m[v[1]]-=v[2];
+         }
+          
+        long long int prev=0;
+        int start=-1;
+        for(auto i:m){
+            
+          
+          
+            
+            if(prev>0){
+                ans.push_back({start,i.first,prev});
+            }
+            prev+=(i.second);
+            start=i.first;
+            
+            
+        }
+        
+        
+        
+        return ans;
+        
+    }
+};
