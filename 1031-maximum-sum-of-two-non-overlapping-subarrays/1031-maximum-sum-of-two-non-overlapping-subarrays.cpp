683c925bf5f114cc9daf7fa915391674e3bfd090
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-02-05 11:13:32 +0530

    Time: 3 ms (88.48%), Space: 8.7 MB (32.27%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,66 +1,64 @@
 class Solution {
 public:
-    int x,y;
-    int solve(int index,vector<int>& nums,int f1,int f2,int dp[][2][2]){
-        int n=nums.size();      
-        if(index>=n)return 0;
-    
+    int f(vector<int>& nums,int a,int b){
         
-        if(dp[index][f1][f2]!=-1)return dp[index][f1][f2];
+        int n=nums.size();
+        vector<int> maxi(n,-1);
         
-        int choice1=solve(index+1,nums,f1,f2,dp);
-        int choice2=0;
-        int choice3=0;
+        int sum=0;
         
-        if(index+x<=n && !f1){
+        for(int i=0;i<a-1;i++){
+            sum+=nums[i];
+        }
+        
+        for(int i=a-1;i<n;i++){
             
-            int sum=0;
+            sum+=nums[i];
             
-            for(int i=index;i<index+x;i++){
-                sum+=nums[i];
+            if(i-1>=0){
+                maxi[i]=max(maxi[i-1],sum);
             }
+            else maxi[i]=sum;
             
-            sum+=solve(index+x,nums,1,f2,dp);
-            choice2=sum;
+            sum-=nums[i-(a-1)];
+            
             
         }
-        if(index+y<=n && !f2){
+        sum=0;
+        
+        for(int i=n-1;i>n-b;i--){
             
-            int sum=0;
-            
-            for(int i=index;i<index+y;i++){
-                sum+=nums[i];
-            }
-            sum+=solve(index+y,nums,f1,1,dp);
-            choice3=sum;
+            sum+=nums[i];
             
         }
         
-        return dp[index][f1][f2]=max({choice1,choice2,choice3});
+        int suff=-1;
+        int ans=0;
+        for(int i=n-b;i>0;i--){
+            
+            sum+=nums[i];
+            
+            suff=max(suff,sum);
+            
+            if(maxi[i-1]!=-1){
+                ans=max(ans,maxi[i-1]+suff);
+            }
+            
+            sum-=nums[i+(b-1)];
+            
+            
+        }
+        
+        
+        
+        return ans;
+        
         
         
     }
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-       x=firstLen;
-       y=secondLen; 
-       int n=nums.size();
-        int dp[n][2][2];
+    int maxSumTwoNoOverlap(vector<int>& nums, int L, int M) {
         
-        for(int i=0;i<n;i++){
-            
-            for(int j=0;j<2;j++){
-                
-                for(int k=0;k<2;k++){
-                    dp[i][j][k]=-1;
-                }
-                
-            }
-            
-            
-        }
-        
-        return solve(0,nums,false,false,dp);
-        
+        return max(f(nums,L,M),f(nums,M,L));
         
     }
 };
