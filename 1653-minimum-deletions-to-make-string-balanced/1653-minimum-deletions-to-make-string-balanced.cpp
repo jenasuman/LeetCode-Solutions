fee06e9264e7347283eea56a9ec176367c93c72c
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-03-12 00:24:07 +0530

    Time: 145 ms (48.61%), Space: 36.8 MB (31.44%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        // int count_b=0;
+        
         int n=s.size();
-        // vector<int> v(n);
-        // vector<int> v2(n);
-        int count_b=0;
+        vector<int> dp(n+1,0);
+        int count=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='b')count_b++;
+            
+            if(s[i]=='b')dp[i+1]=dp[i],count++;
+            else{
+                //We have now two choices 
+                //either to remove that 'a' or remove all 'b' before it
                 
+                dp[i+1]=min(dp[i]+1,count);
+            }
         }
-        int count_a=0;
-        int ans=INT_MAX;
-        
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='b')count_b--;
-            ans=min(ans,count_a+count_b);
-            if(s[i]=='a')count_a++;
-            
-            
-        }
-        
-        return ans;
+        return dp[n];
         
     }
 };
