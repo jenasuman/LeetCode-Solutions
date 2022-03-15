d2b13097b584549a260e70733a9aa1642e0304cc
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-03-15 10:21:49 +0530

    Time: 113 ms (59.65%), Space: 27.2 MB (41.99%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        if(s.size()&1)return false;
+        // cout<<s.size()<<"\n";
+        if(s.size()%2!=0)return false;
         int lopen=0,free=0,lclose=0;
         for(int i=0;i<s.size();i++){
             if(locked[i]=='0') free++;
@@ -10,8 +11,10 @@ public:
                 else 
                     lclose++;
             }
+            // cout<<free<<"\n";
             if(lclose>free+lopen) return false;
         }
+        
           lopen=0,free=0,lclose=0;
         for(int i=s.size()-1;i>=0;i--){
             if(locked[i]=='0') free++;
