fd21cdc7764eb98e0febfb93fd48975b73ad47ca
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-03-26 00:44:16 +0530

    Time: 3 ms (90.96%), Space: 8.1 MB (33.00%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int dp[101][101][101];
-    int solve(int index,vector<vector<int>>& cost,int n,int group1,int group2){
-        
-        if(index>=n){
-            return 0;
-        }
-        if(dp[index][group1][group2]!=-1)return dp[index][group1][group2];
-        int case1=INT_MAX;
-        int case2=INT_MAX;
-        
-        if(group1!=0){
-            case1=cost[index][0]+solve(index+1,cost,n,group1-1,group2);
-        }
-        if(group2!=0){
-          case2=cost[index][1]+solve(index+1,cost,n,group1,group2-1);            
-        }
-        
-        
-        return dp[index][group1][group2]=min(case1,case2);
-        
-        
-    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n=costs.size();
-        int m=n/2;
-        memset(dp,-1,sizeof(dp));
-        return solve(0,costs,n,m,m);
         
+        int cost=0;
+        vector<int> diff;
+        for(auto v:costs){
+            
+            cost+=v[0];
+            
+            diff.push_back(v[1]-v[0]);
+            
+            
+        }
+        sort(diff.begin(),diff.end());
+        
+        int n=costs.size();
+        
+        for(int i=0;i<n/2;i++){
+            cost+=diff[i];
+        }
+        
+        return cost;
         
     }
 };
