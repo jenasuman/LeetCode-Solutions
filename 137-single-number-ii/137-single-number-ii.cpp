71467cb890a7ab3fc81890070ae94a24abb0c292
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-03-28 10:50:08 +0530

    Time: 7 ms (88.63%), Space: 9.6 MB (57.55%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one, two;
+        one = 0;
+        two = 0;
+		
+		for(int i=0; i<nums.size(); i++){
+            
+           one=(one^nums[i])&(~two);
+           two=(two^nums[i])&(~one); 
+            
+        }
+        return one;
+        
+        
+    }
+};
