211787fe246434a694bd4fcca4491c549ae58a45
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-04-21 09:18:11 +0530

    Time: 216 ms (28.74%), Space: 41.7 MB (57.84%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,27 @@
+class MyHashSet {
+    map<int,int> m;
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        m[key]++;
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+    
+    bool contains(int key) {
+        return m.find(key)==m.end()?false:true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
