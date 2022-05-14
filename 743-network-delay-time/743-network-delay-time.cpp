62ec89a34657027055b658ad5bbbcf403348095b
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-05-14 16:54:45 +0530

    Time: 165 ms (50.82%), Space: 43.7 MB (22.22%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        
-        unordered_map<int,vector<pair<int,int>>> adj;
+        unordered_map<int,vector<pair<int,int>>> graph;
         
         for(auto v:times){
-            adj[v[0]].push_back({v[1],v[2]});
+            graph[v[0]].push_back({v[1],v[2]});
         }
-        
-        queue<int> q;
-        
-        q.push(k);
         
         vector<int> time(n+1,INT_MAX);
         
         time[0]=0;
-        time[k]=0;
         
-        while(!q.empty()){
-            
-            int t=q.front();
-            
-            q.pop();
-            
-            for(auto p:adj[t]){
-                int node=p.first;
-                
-                if(time[node]>time[t]+p.second){
-                    time[node]=time[t]+p.second;
-                    q.push(node);
+        queue<int> pq;
+        time[k]=0;
+        pq.push(k);
+        
+        while(!pq.empty()){
+            auto p=pq.front();
+            pq.pop();
+          
+            for(auto c:graph[p]){
+                int x=c.second;
+                int y=c.first;
+                if(time[y]>time[p]+x){
+                time[y]=time[p]+x;    
+                pq.push(y);
                 }
-                
-                
             }
             
             
@@ -42,10 +36,10 @@ public:
         int ans=INT_MIN;
         
         for(auto i:time){
+            // cout<<i<<" ";
             if(i==INT_MAX)return -1;
-            ans=max(ans,i);
+            ans=max(i,ans);
         }
         return ans;
-        
     }
 };
