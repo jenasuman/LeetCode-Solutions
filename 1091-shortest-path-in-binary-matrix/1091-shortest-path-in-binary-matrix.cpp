46b0b5a934c69f32c7ddaeb558511ba77be1537b
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-05-16 09:52:41 +0530

    Time: 57 ms (92.91%), Space: 20.2 MB (59.11%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,55 +1,49 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int dx[8]={0,0,-1,1,1,-1,1,-1};
+               int dx[8]={0,0,-1,1,1,-1,1,-1};
         int dy[8]={-1,1,0,0,1,-1,-1,1};
         
-        queue<pair<int,int>> q;
-
+        queue<pair<int,pair<int,int>>> q;
         int n=grid.size();
         int m=grid[0].size();
         
         if(grid[0][0]==1 || grid[n-1][m-1]==1)return -1;
         
         
-        q.push({0,0});
-        grid[0][0]=1;
-        int cnt=0;
+        q.push({1,{0,0}});
         
+        grid[0][0]=1;
         while(!q.empty()){
-            cnt++;
             
-            int l=q.size();
+            auto curr=q.front().second;
+    
+            int currX=curr.first;
+            int currY=curr.second;
+            // grid[currX][currY]=1;
+            int dis=q.front().first;
+            if(currX==n-1 && currY==m-1)return dis;
+            q.pop();
+            // cout<<currX<<" "<<currY<<" "<<dis<<"\n";
+            for(int i=0;i<8;i++){
+                int x=currX+dx[i];
+                int y=currY+dy[i];
             
-            for(int i=0;i<l;i++){
-                
-                auto p=q.front();
-                q.pop();
-                
-                int x=p.first;
-                int y=p.second;
-                
-                if(x==n-1 && y==m-1)return cnt;
-                
-                for(int j=0;j<8;j++){
-                    int currX=x+dx[j];
-                    int currY=y+dy[j];
-                    
-                    
-              if(currX<n && currX>=0 && currY>=0 && currY<m && grid[currX][currY]==0){
-                  grid[currX][currY]=1;
-                  q.push({currX,currY});
-              }
-                    
-                    
+                if(x>=0 && x<n && y>=0 && y<m && grid[x][y]!=1)
+                {   
+                    // if(x==n-1 && y==m-1)return dis+1;
+                    q.push({dis+1,{x,y}});
+                    grid[x][y]=1;
                 }
-                
                 
                 
             }
             
+            
+            
+            
+            
         }
         return -1;
-        
     }
 };
