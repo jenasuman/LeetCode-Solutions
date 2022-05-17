755c2d0a1529e8233d2b0787501b669f1296c2a6
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-05-17 20:14:12 +0530

    Time: 78 ms (6.09%), Space: 27.4 MB (45.03%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int badL = 0;
-        int badR = 0;
+        vector<int> temp(nums);
         
-        int n = nums.size();
+        // for(auto i:temp)cout<<i<<" ";
         
-        //first number that is not in ascending order
-        for(int i = 1; i < n; i++) {
-            if(nums[i] < nums[i-1]) {
-                badL = i-1;
-                break;
+        int firstDiff=-1;
+        int lastDiff=-1;
+        
+        sort(temp.begin(),temp.end());
+        
+        int n=temp.size();
+        
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]!=temp[i]){
+                if(firstDiff==-1){
+                    firstDiff=i;
+                    lastDiff=i;
+                }
+                else lastDiff=i;
             }
-        }    
-        
-        //last number that is not in ascending order
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] > nums[i+1]) {
-                badR = i+1;
-                break;
-            }
+            
+            
+            
         }
         
-        //find the min and max values within the range of not ascending order
-        int mx = INT_MIN;
-        int mn = INT_MAX;
-
-        for(int i = badL; i <= badR; i++) {
-            mx = max(nums[i], mx);
-            mn = min(nums[i], mn);
-        }
+        if(firstDiff==-1)return 0;
         
-        //already sorted, return 0
-        if(badL == badR) return 0;
         
-        //size of the subarray answer
-        int ans = badR - badL + 1;
         
-        int left = badL;
-        while(left-1 >= 0 && mn < nums[left-1]) {
-            ans++;
-            left--;
-        }
+        return lastDiff-firstDiff+1;
         
-        while(badR+1 < n && mx > nums[badR+1]) {
-            ans++;
-            badR++;
-        }
         
-        return ans;
+        
     }
 };
