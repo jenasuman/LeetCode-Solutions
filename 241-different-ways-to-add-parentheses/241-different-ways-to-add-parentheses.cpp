f6fc632c53311c60beedf004ec31157be30e83d3
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-05-20 00:44:41 +0530

    Time: 3 ms (75.56%), Space: 11.5 MB (38.06%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-     int perform(int x, int y, char op) {
-        if(op == '+') return x + y;
-        if(op == '-') return x - y;
-        if(op == '*') return x * y;
-        return 0;
-    }
-    
-    vector<int> diffWaysToCompute(string exp) {
+    vector<int> diffWaysToCompute(string input) {
         
-        vector<int> results;
-        bool isNumber = 1;
-    
-        for(int i = 0; i < exp.length(); i++) {
-            // check if current character is an operator
-            if(!isdigit(exp[i])) {
-                
-                // if current character is not a digit then
-                // exp is not purely a number
-                isNumber = 0;
-                
-                // list of first operands
-                vector<int> left = diffWaysToCompute(exp.substr(0, i));
-                
-                // list of second operands
-                vector<int> right = diffWaysToCompute(exp.substr(i + 1));
-                
-                // performing operations
-                for(int x : left) {
-                    for(int y : right) {
-                        int val = perform(x, y, exp[i]);
-                        results.push_back(val);
+       vector<int> result;
+        int size = input.size();
+        for (int i = 0; i < size; i++) {
+            char cur = input[i];
+            if (cur == '+' || cur == '-' || cur == '*') {
+                // Split input string into two parts and solve them recursively
+                vector<int> result1 = diffWaysToCompute(input.substr(0, i));
+                vector<int> result2 = diffWaysToCompute(input.substr(i+1));
+                for (auto n1 : result1) {
+                    for (auto n2 : result2) {
+                        if (cur == '+')
+                            result.push_back(n1 + n2);
+                        else if (cur == '-')
+                            result.push_back(n1 - n2);
+                        else
+                            result.push_back(n1 * n2);    
                     }
                 }
-                
             }
         }
+        // if the input string contains only number
+        if (result.empty())
+            result.push_back(atoi(input.c_str()));
+        return result;
         
-        if(isNumber == 1) results.push_back(stoi(exp));
-        return results;
     }
 };
