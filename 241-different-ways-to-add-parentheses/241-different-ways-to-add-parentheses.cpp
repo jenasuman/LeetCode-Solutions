6680ccadaabef4ccf967833d7f313f6dc601d949
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-05-20 00:48:45 +0530

    Time: 11 ms (17.13%), Space: 11.4 MB (38.06%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string input) {
+    vector<int> diffWaysToCompute(string s) {
         
-       vector<int> result;
-        int size = input.size();
-        for (int i = 0; i < size; i++) {
-            char cur = input[i];
-            if (cur == '+' || cur == '-' || cur == '*') {
-                // Split input string into two parts and solve them recursively
-                vector<int> result1 = diffWaysToCompute(input.substr(0, i));
-                vector<int> result2 = diffWaysToCompute(input.substr(i+1));
-                for (auto n1 : result1) {
-                    for (auto n2 : result2) {
-                        if (cur == '+')
-                            result.push_back(n1 + n2);
-                        else if (cur == '-')
-                            result.push_back(n1 - n2);
-                        else
-                            result.push_back(n1 * n2);    
+        vector<int> results;
+        
+        int n=s.size();
+        bool flag=true;
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i]=='+' || s[i]=='-' || s[i]=='*'){
+                
+                flag=false;
+                vector<int> left=diffWaysToCompute(s.substr(0,i));
+                vector<int> right=diffWaysToCompute(s.substr(i+1));
+                
+                
+                for(auto k:left){
+                    
+                    for(auto j:right){
+                        
+                         if(s[i]=='+'){
+                             results.push_back(k+j);
+                         }    
+                         else if(s[i]=='-'){
+                             results.push_back(k-j);
+                         }
+                        else results.push_back(k*j);
+                        
+                        
                     }
                 }
+                
+                
+                
+                
+                
+                
+                
+                
             }
+            
+            
+            
+            
         }
-        // if the input string contains only number
-        if (result.empty())
-            result.push_back(atoi(input.c_str()));
-        return result;
+        
+        if(flag){
+            results.push_back(stoi(s));
+        }
+        
+        return results;
         
     }
 };
