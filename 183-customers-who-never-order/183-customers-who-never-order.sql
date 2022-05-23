cfb74c827aeaa705600bed55ad5e062129421be4
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-05-23 23:58:15 +0530

    Time: 1224 ms (12.82%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,3 +1,5 @@
 /* Write your PL/SQL query statement below */
 
-select name as Customers from Customers where Customers.id NOT IN (select customerId from Orders) 
+-- select name as Customers from Customers where Customers.id NOT IN (select customerId from Orders) 
+
+select name as Customers from Customers LEFT JOIN Orders on Customers.id=Orders.customerId where Orders.customerId IS NULL
