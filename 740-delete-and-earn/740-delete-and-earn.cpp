560e675f3f6d0fd2b699636860aae83e3900f4a2
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-05-26 19:57:30 +0530

    Time: 13 ms (78.31%), Space: 11.8 MB (84.22%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    int dp[10001];
-    int solve(int index,vector<int>& v){
-        
-        if(index>=v.size())return 0;
-        if(dp[index]!=-1)return dp[index];
-        return dp[index]=max((v[index]+solve(index+2,v)),solve(index+1,v));
-        
-    }
     int deleteAndEarn(vector<int>& nums) {
-            vector<int> v(10001,0);
-            memset(dp,-1,sizeof(dp));
-            for(auto i:nums){
-                v[i]+=i;
-            }
         
-            return solve(0,v);
-             
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+        //House Robber Type problem if we can sum the common terms
+        
+        vector<int> v;
+        int sum=nums[0];
+        for(int i=1;i<n;i++){
+            
+            if(nums[i]!=nums[i-1]){
+                v.push_back(sum);
+                sum=0;
+                if(nums[i]-nums[i-1]!=1){
+                    v.push_back(0);
+                }
+            }
+            sum+=nums[i];
+            
+        }
+        v.push_back(sum);
+        n=v.size();
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=v[0];
+        
+        for(int i=2;i<=n;i++){
+            
+            dp[i]=max(v[i-1]+dp[i-2],dp[i-1]);
+            
+            
+        }
+        
+        return dp[n];
+        
+        
+        
+        
+        
     }
 };
