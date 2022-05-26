9006a34bffd333ed8246f2db42f66cc4a68df41d
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-05-27 00:15:34 +0530

    Time: 292 ms (64.67%), Space: 88.2 MB (71.98%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,31 +1,40 @@
-
 class SnapshotArray {
-    public:
-vector<vector<pair<int, int>>> data;
-    int snapIdx;
-    
+      vector<vector<pair<int,int>>> v;
+      int id;
+ public:
     SnapshotArray(int length) {
-        data.resize(length);
-        snapIdx = 0;
+       id=0;
+       v.resize(length);  
     }
     
     void set(int index, int val) {
-        if (data[index].empty() || data[index].back().first < snapIdx)
-            data[index].emplace_back(snapIdx, val);
-        else
-            data[index].back().second = val;
+        
+          if(v[index].empty() || v[index].back().first<id){
+              v[index].emplace_back(id,val);
+            }
+        
+          else v[index].back().second=val; 
+        
+        
+        
     }
     
     int snap() {
-        return snapIdx++;
+       return id++;
     }
     
     int get(int index, int snap_id) {
-        auto it = upper_bound(data[index].begin(), data[index].end(), make_pair(snap_id, INT_MAX));
-        if (it == data[index].begin())  // no data at snap_id
+      
+        auto it=upper_bound(v[index].begin(),v[index].end(),make_pair(snap_id,INT_MAX));
+        
+        
+        if(it==v[index].begin()){
             return 0;
-        else    // return the data with snapIdx <= snap_id, which is the prev of upper_bound
-            return prev(it)->second;
+        }
+        
+        else return prev(it)->second;
+        
+        
     }
 };
 
