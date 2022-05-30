514107fe0e6d09ac1eb964825460cc4a1e81b728
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-05-31 00:47:10 +0530

    Time: 939 ms (26.33%), Space: 0B (100.00%) - LeetHub

--- 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
+-- SELECT event_day AS day, emp_id,SUM((out_time-in_time)) AS total_time 
+-- FROM Employees 
+-- GROUP BY event_day,emp_id;
 
 
 SELECT 
-event_day AS day, emp_id, SUM((out_time - in_time)) AS total_time
+to_char(event_day,'YYYY-MM-DD') AS day, emp_id, SUM((out_time - in_time)) AS total_time
 FROM Employees
-GROUP BY event_day, emp_id;
+GROUP BY to_char(event_day,'YYYY-MM-DD'), emp_id;
