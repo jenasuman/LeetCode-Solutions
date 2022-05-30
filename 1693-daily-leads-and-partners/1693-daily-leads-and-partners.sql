d2dafbd7f58e8b660b91e5600d940dd0a726b11a
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-05-31 01:06:36 +0530

    Time: 811 ms (54.23%), Space: 0B (100.00%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -1,8 +1,7 @@
-# Write your MySQL query statement below
-
+/* Write your PL/SQL query statement below */
 SELECT 
-date_id,make_name,
+to_char(date_id,'YYYY-MM-DD') AS date_id,make_name,
 COUNT(DISTINCT(lead_id)) AS unique_leads,
 COUNT(DISTINCT(partner_id)) AS unique_partners
 FROM DailySales 
-GROUP BY date_id,make_name;
+GROUP BY to_char(date_id,'YYYY-MM-DD'),make_name;
