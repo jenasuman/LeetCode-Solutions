e6985f88b2e7b8457769a557c350e4146711dc4e
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-04 22:17:28 +0530

    Time: 60 ms (35.80%), Space: 16.4 MB (36.43%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    static bool comp(pair<int,int>& p1,pair<int,int>& p2){
+        if(p1.first==p2.first)return p1.second>p2.second;
+        return p1.first<p2.first;
+    }
+public:
+    
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        
+        vector<pair<int,int>> v;
+        
+        for(auto u:buildings){
+            v.push_back({u[0],u[2]});
+            v.push_back({u[1],(-1*u[2])});
+        }
+        
+        sort(v.begin(),v.end(),comp);
+        // for(auto i:v){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+        vector<vector<int>> ans;
+        multiset<int> s;
+        s.insert(0);
+        int prev=0;
+        
+        for(auto [x,h]:v){
+            
+           if(h<0){
+               
+               
+               
+               auto it=s.find((-1*h));
+               
+               // cout<<*it<<" ";
+               if(it!=s.end())s.erase(it);
+               int curr=*(s.rbegin());
+               if(prev>curr){
+                   prev=curr;
+                   ans.push_back({x,prev});
+               }
+               
+           } 
+           // auto currHeight=s.rbegin(); 
+           if(prev<h){
+               prev=h;
+               ans.push_back({x,h});
+           }    
+           // cout<<prev<<" ";
+            s.insert(h);
+            
+            
+            
+            
+        }
+        
+        return ans;
+        
+    }
+};
