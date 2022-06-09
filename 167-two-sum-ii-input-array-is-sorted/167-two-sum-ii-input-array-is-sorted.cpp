020148858b270499bc87827b137d7841b7c2d772
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-09 20:38:58 +0530

    Time: 16 ms (67.85%), Space: 15.5 MB (93.11%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        
-        // unordered_map<int,int> m;
-        int n=numbers.size();
-        
-        //Can you do it in constant space????
-        
-        for(int i=0;i<n-1;i++){
-            
-            int currTarget=target-numbers[i];
-            
-            
-            int index=lower_bound(begin(numbers)+i+1,end(numbers),currTarget)-begin(numbers);
-            
-            if(index!=n && numbers[index]==currTarget){
-                
-                return {i+1,index+1};
-                
-                
-            }
-            
-            
-            
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            int sum = nums[i] + nums[j];
+            if(sum==target) return {i+1,j+1};
+            else if(sum>target) j--;
+            else i++;
         }
-         
-        
-        
-        return {};
-        
+        return {}; // not found
     }
+        
+    
 };
