670ab82f0fc6904dba13be1317492281c3683a26
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-13 22:57:23 +0530

    Time: 207 ms (17.35%), Space: 49.3 MB (10.85%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,70 @@
 class Solution {
-    int n;
-    int dp[13][10002];
-    int f(int i,vector<int>& coins,int amount){
+//     int n;
+//     int dp[14][10004];
+//     int f(int i,vector<int>& coins,int amount){
         
-        if(amount==0)return 0;
-        if(i<0 && amount){
-            return INT_MAX;
+//         if(amount==0)return 0;
+//         if(i<0 && amount){
+//             return INT_MAX;
+//         }
+//         if(dp[i][amount]!=-1)return dp[i][amount];
+//         int take=INT_MAX;
+//         int notTake=INT_MAX;
+        
+//         if(coins[i]<=amount){
+            
+//             int ans=f(i,coins,amount-coins[i]);
+//             take=(ans!=INT_MAX)?(1+ans):INT_MAX;
+            
+//         }
+//         notTake=f(i-1,coins,amount);
+        
+//         return dp[i][amount]=min(take,notTake);
+//     } 
+    
+    
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // memset(dp,-1,sizeof(dp));
+        int n=coins.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX));
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
-        if(dp[i][amount]!=-1)return dp[i][amount];
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int a=1;a<=amount;a++){
+                
         int take=INT_MAX;
         int notTake=INT_MAX;
         
-        if(coins[i]<=amount){
+        if(coins[i-1]<=a){
             
-            int ans=f(i,coins,amount-coins[i]);
+            int ans=dp[i][a-coins[i-1]];
             take=(ans!=INT_MAX)?(1+ans):INT_MAX;
             
         }
-        notTake=f(i-1,coins,amount);
+        notTake=dp[i-1][a];
+                
+        dp[i][a]=min(take,notTake);        
+                
+                
+                
+                
+                
+                
+       }
+            
+            
+            
+            
+        }
         
-        return dp[i][amount]=min(take,notTake);
-    } 
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        n=coins.size();
-        int i=n-1;
-        int ans=f(i,coins,amount);
-        return ans==INT_MAX?-1:ans;
+        
+        
+        return dp[n][amount]==INT_MAX?-1:dp[n][amount];
     }
 };
