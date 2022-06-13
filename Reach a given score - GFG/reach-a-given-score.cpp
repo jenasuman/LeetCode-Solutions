994494c8311da47a0f2bcd0d64b4cb7f4bdedeb6
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-13 11:58:51 +0530

    Added solution - LeetHub

---
+++ Reach a given score - GFG/reach-a-given-score.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ // } Driver Code Ends
+// Complete this function
+
+long long int f(long long int n,long long int i,long long int arr[]){
+    
+    if(n==0){
+        return 1;
+    }
+    
+    if(i==0 || n<0)return 0;
+    
+    long long int pick=(arr[i-1]<=n)?f(n-arr[i-1],i,arr):0;
+    long long int notPick=f(n,i-1,arr);
+    
+    return pick+notPick;
+    
+}
+
+long long int count(long long int n)
+{
+      long long int dp[1001];
+      
+      memset(dp,-1,sizeof(dp));
+      
+      long long int arr[3]={3,5,10};
+      
+      return f(n,3,arr);
+}
+
+// { Driver Code Starts.
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		cout<<count(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
