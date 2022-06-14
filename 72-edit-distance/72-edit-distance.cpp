efce8ddb6df6a1fc5fd3cadf37073595dcf737ae
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-15 01:40:32 +0530

    Time: 23 ms (43.84%), Space: 8.4 MB (67.23%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -40,42 +40,46 @@ public:
      // int j=m-1;
      // memset(dp,-1,sizeof(dp));  
      
-     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+     // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+      vector<int> dp(m+1,0);  
         
-     dp[0][0]=0;
+     dp[0]=0;
         
-     for(int i=1;i<=n;i++){
+//      for(int i=1;i<=n;i++){
          
-         dp[i][0]=i;
+//          dp[i][0]=i;
          
-     }   
+//      }   
      for(int i=1;i<=m;i++){
          
-         dp[0][i]=i;
+         dp[i]=i;
          
      }   
      
     for(int i=1;i<=n;i++){
+        vector<int> temp(m+1,0);
+        temp[0]=i;
         for(int j=1;j<=m;j++){
             
          if(word1[i-1]==word2[j-1]){
             
-             dp[i][j]=dp[i-1][j-1];
+             temp[j]=dp[j-1];
     
         }
         else{
-        int replaceCase=1+dp[i-1][j-1];
-        int deleteCase=1+dp[i-1][j];
-        int insertCase=1+dp[i][j-1];
-        dp[i][j]=min({replaceCase,deleteCase,insertCase}); 
+        int replaceCase=1+dp[j-1];
+        int deleteCase=1+dp[j];
+        int insertCase=1+temp[j-1];
+        temp[j]=min({replaceCase,deleteCase,insertCase}); 
         }    
             
             
         }
+        dp=temp;
     }    
         
        
-    return dp[n][m];   
+    return dp[m];   
          
          
         
