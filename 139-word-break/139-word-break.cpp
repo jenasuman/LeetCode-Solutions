e7d1dc1671faddc5b55737cec90b4fbc692fb446
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-17 20:20:50 +0530

    Time: 36 ms (22.53%), Space: 12.9 MB (50.00%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,46 +1,63 @@
 class Solution {
     unordered_map<string,int> m;
-    int dp[302][302];
+//     int dp[302][302];
     
-    bool f(int i,int j,string& s){
+//     bool f(int i,int j,string& s){
         
-        if(i>j){
-            return false;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
+//         if(i>j){
+//             return false;
+//         }
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        string currSubstring=s.substr(i,j-i+1);
+//         string currSubstring=s.substr(i,j-i+1);
         
-        if(m.find(currSubstring)!=m.end())return dp[i][j]=true;
+//         if(m.find(currSubstring)!=m.end())return dp[i][j]=true;
         
-        for(int k=i;k<j;k++){
+//         for(int k=i;k<j;k++){
             
-            bool case1=f(i,k,s);
-            bool case2=f(k+1,j,s);
+//             bool case1=f(i,k,s);
+//             bool case2=f(k+1,j,s);
             
-            if(case1 and case2){
-                return dp[i][j]=true;
-            }
+//             if(case1 and case2){
+//                 return dp[i][j]=true;
+//             }
             
-        }
+//         }
         
-        return dp[i][j]=false;
+//         return dp[i][j]=false;
         
-    }
+//     }
     
     
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        m.clear(); 
+    bool wordBreak(string s, vector<string>& word) {
+         
+        int n=s.size();
         
-        for(auto w:wordDict){
-              m[w]++;
+        vector<bool> dp(n+1);
+        
+        dp[0]=true;
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=0;j<i;j++){
+                
+                if(dp[j] && find(begin(word),end(word),s.substr(j,i-j))!=end(word)){
+                    dp[i]=true;
+                    break;
+                }
+                
+                
+                
+                
+            }
+            
+            
+            
+            
         }
-        int i=0;
-        int j=s.size()-1;
-        memset(dp,-1,sizeof(dp));
-        return f(i,j,s);
         
+        return dp[n];
         
         
     }
