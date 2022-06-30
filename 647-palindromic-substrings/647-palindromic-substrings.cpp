56a4628863ff83706c2ccd54f0d1df855e09aed9
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-06-30 20:55:29 +0530

    Time: 981 ms (6.97%), Space: 457 MB (5.10%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -15,22 +15,27 @@ public:
         
         int n=s.size();
         
-        vector<int> dp(n,1);
+        vector<int> dp(n+1,1);
         
-        int ans=0;
-        
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
+            
             dp[i]+=dp[i-1];
-            for(int j=i-1;j>=0;j--){
+            string temp="";
+            // dp[i]++;
+            temp.push_back(s[i-1]);
+            for(int j=i;j<n;j++){
                 
-                string t=s.substr(j,i-j+1);
-                if(isPalindrone(t)){
-                    dp[i]+=1;
+                temp.push_back(s[j]);
+                
+                if(isPalindrone(temp)){
+                    dp[i]++;
                 }
                 
-            }
+             }
             
-      
+            
+            
+            
         }
         
         return dp[n-1];
