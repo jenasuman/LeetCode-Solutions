7a0802c50068ec3baa9b23dd90b67b7ece965821
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-02 10:19:32 +0530

    Time: 31 ms (10.66%), Space: 11.4 MB (11.89%) - LeetHub

---
+++ 955-delete-columns-to-make-sorted-ii/955-delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+        
+        
+        int n=strs.size();
+        int m=strs[0].size();
+        
+        if(n==1){
+            return 0;
+        }
+        int count=0;
+        unordered_map<int,int> col;
+        while(1){
+            bool flag=true;
+        for(int i=1;i<n;i++){
+            
+            string s1=strs[i-1];
+            string s2=strs[i];
+            
+            for(int j=0;j<m;j++){
+                
+                if(col.find(j)==col.end()){
+                
+                if(s1[j]==s2[j]){
+                    continue;
+                }
+                if(s1[j]>s2[j]){
+                     flag=false;
+                     col[j]++;
+                }
+                if(s1[j]<s2[j]){
+                     break; 
+                }    
+                
+                }
+                
+                
+            }
+            
+            
+            
+            
+             
+         }   
+            
+        if(flag==true)break;
+        }    
+            
+            
+
+        
+        
+        
+        return col.size();
+        
+    }
+};
