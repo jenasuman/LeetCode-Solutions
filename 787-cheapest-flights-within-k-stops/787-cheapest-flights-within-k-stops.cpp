d66e2bac6bfa5389e0bd329ab2fdae252bde36ca
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-05 18:29:42 +0530

    Time: 54 ms (47.10%), Space: 13.6 MB (55.40%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,68 +1,45 @@
 class Solution {
+    unordered_map<int,vector<pair<int,int>>> adj;
+    
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int dp[100][100];
+    int f(int src,int dst,int k){
+         
+
         
-        unordered_map<int,vector<pair<int,int>>> adj;
+        if(src==dst){
+            return 0;
+        }
+        
+        if(k<0){
+            return INT_MAX;
+        }
+        if(dp[src][k]!=-1)return dp[src][k];
+        int ans=INT_MAX;
+                    
+        for(auto c:adj[src]){
+            
+            int curr=c.first;
+            int price=c.second;
+            int res=f(curr,dst,k-1);
+            // cout<<res<<"\n";
+            if(res!=INT_MAX)
+            ans=min(ans,price+res);
+            
+            
+        }
+        
+        return dp[src][k]=ans;
+        
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
         for(auto v:flights){
             adj[v[0]].push_back({v[1],v[2]});
         }
-        
-        vector<int> price(n,INT_MAX);
-        
-        int countStops=0;
-        
-        queue<pair<int,int>> q;
-        q.push({src,0});
-        int ans=INT_MAX;
-        
-        while(!q.empty() && countStops<=k){
-            
-            int n=q.size();
-            
-            for(int i=0;i<n;i++){
-                
-                auto t=q.front();
-                
-                int s=t.first;
-                int p=t.second;
-                
-                q.pop();
-                
-                if(p>price[s])continue;
-                
-                price[s]=p;
-                
-                
-                
-                for(auto c:adj[s]){
-                    
-                    
-                    
-                   if(ans<p+c.second)continue;
-                    
-                   if(c.first==dst)ans=min(ans,price[s]+c.second);
-                   
-                    q.push({c.first,price[s]+c.second});
-                
-                    
-                    
-                }
-                
-                
-                
-                
-                
-            }
-            countStops++;
-            
-            
-            
-            
-            
-        }
-        
-        return ans!=INT_MAX?ans:-1;
+        memset(dp,-1,sizeof(dp));
+        int ans=f(src,dst,k);
+        return ans==INT_MAX?-1:ans;
         
     }
 };
