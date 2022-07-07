3758cc10e702fbee4eebd28ca183863f4d880679
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-07 19:32:45 +0530

    Time: 4 ms (73.94%), Space: 6.4 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[101][101][201];
+    int dp[101][101];
     bool f(int i,int j,int k,string& s1,string& s2,string& s3){
         
         if(k<0 && i<0 && j<0){
@@ -23,25 +23,25 @@ public:
             return temp1==temp2;
             
         }
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
+        if(dp[i][j]!=-1)return dp[i][j];
         if(s1[i]==s3[k] && s2[j]==s3[k]){
             
-            return dp[i][j][k]=f(i-1,j,k-1,s1,s2,s3)||f(i,j-1,k-1,s1,s2,s3);
+            return dp[i][j]=f(i-1,j,k-1,s1,s2,s3)||f(i,j-1,k-1,s1,s2,s3);
             
         }
         else if(s1[i]==s3[k]){
             
-            return dp[i][j][k]=f(i-1,j,k-1,s1,s2,s3);
+            return dp[i][j]=f(i-1,j,k-1,s1,s2,s3);
             
         }
         else if(s2[j]==s3[k]){
             
-            return dp[i][j][k]=f(i,j-1,k-1,s1,s2,s3);
+            return dp[i][j]=f(i,j-1,k-1,s1,s2,s3);
             
         }
         
         
-        return dp[i][j][k]=false;
+        return dp[i][j]=false;
         
         
         
