ba8128536424c114bb5ceaf22965b3bece59958a
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-11 21:04:32 +0530

    Time: 103 ms (41.61%), Space: 5.9 MB (90.53%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int dp[1001][1001][3];
-    int f(int ground,int top,int n){
-      
-        if(top-ground+1<=1){
-            return top-ground+1;
-        }
+    int dp[1001][3];
+    int f(int floors,int eggs){
         
-        if(n==1){
-            return top-ground+1;
-        }
-        if(dp[ground][top][n]!=-1)return dp[ground][top][n];
+        if(dp[floors][eggs]!=-1)return dp[floors][eggs];
         
-      
-        int count=INT_MAX;
-        for(int i=ground;i<=top;i++){
+        if(eggs==1)return floors;
+        
+        if(floors<=1)return floors;
+        
+        int ans=1e6;
+        
+        for(int i=1;i<=floors;i++){
+            //Break
+            int case1=f(i-1,eggs-1);
+            //Not break
+            int case2=f(floors-i,eggs);
             
-            int case1=f(i+1,top,n);
-            int case2=f(ground,i-1,n-1);
+            ans=min(ans,max(case1,case2)+1);
             
-            int currCount=1+max(case1,case2);
-            count=min(count,currCount);
-        }
+         }
         
-        return dp[ground][top][n]=count;
-        
+        return dp[floors][eggs]=ans;
         
     }
     
@@ -32,7 +29,7 @@ public:
         
         //two eggs 
         memset(dp,-1,sizeof(dp));
-        return f(1,n,2);
+        return f(n,2);
          
         
     }
