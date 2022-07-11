210e4b2f43d6b5a5ac5cf001bc7ee0ca267aebbe
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-11 22:20:41 +0530

    Time: 604 ms (57.75%), Space: 259.2 MB (52.06%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -3,16 +3,17 @@ public:
     int dp[500005][4];
     int f(int i,int lane,int n,vector<int>& obs){
         
-        if(i==n-2)return 0;
-
+        if(i==n)return 0;
+         
+        if(obs[i]==lane)return 1e6;
         if(dp[i][lane]!=-1)return dp[i][lane];
         
         int case2=1e6,case3=1e6,case1=1e6;
         
-        if(obs[i+1]!=lane){
+        // if(obs[i+1]!=lane){
             case1=f(i+1,lane,n,obs);
-        }
-        else{
+        // }
+        // else{
         if(lane==2){
             if(obs[i]!=1)case2=f(i+1,1,n,obs);
             if(obs[i]!=3)case3=f(i+1,3,n,obs);
@@ -28,7 +29,7 @@ public:
             if(obs[i]!=2)case3=f(i+1,2,n,obs);
             case2=min(case3,case2); 
         }
-        }
+        
         return dp[i][lane]=min(case1,case2+1);
         
         
