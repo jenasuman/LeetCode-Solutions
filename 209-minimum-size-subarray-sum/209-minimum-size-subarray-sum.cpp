fbaab3bd0c249b5ee69e60a20ee7f4ef234ecded
Suman Jena <95709987+jenasuman@users.noreply.github.com>
2022-07-16 00:54:53 +0530

    Time: 50 ms (33.33%), Space: 28.1 MB (38.13%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -3,49 +3,78 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         
         int n=nums.size();
-        for(int i=1;i<n;i++){
-            nums[i]+=nums[i-1];
+//         for(int i=1;i<n;i++){
+//             nums[i]+=nums[i-1];
+//         }
+        
+//         if(nums[n-1]<target)return 0;
+        
+//         int ans=n;
+//         // O(nlog(n))
+//         for(int i=0;i<n;i++){
+            
+//             if(nums[i]<target){
+//                 continue;
+//             }
+            
+//             int r=nums[i]-target;
+            
+//             int index=lower_bound(begin(nums),begin(nums)+i,r)-begin(nums);
+
+//             if(index==i){
+//                 ans=min(ans,i-index+1);
+//             }
+//             if(index!=0){
+//                 if(nums[index]==r){
+//                     ans=min(ans,i-index);
+//                 }
+//                 else{
+//                     ans=min(ans,i-index+1);
+//                 }
+//             }
+//             if(index==0){
+                
+//                 if(nums[index]==r){
+//                   ans=min(ans,i-index);
+//                 }
+//                 else ans=min(ans,i+1);
+                
+                
+//             }
+               
+            
+//         }
+        
+//         return ans;
+        
+        //Sliding Window
+        
+        int i=0;
+        int j=0;
+        
+        int sum=0;
+        
+        int ans=INT_MAX;
+        
+        for(;i<n;i++){
+            
+            sum+=nums[i];
+            
+            while(sum>=target && j<=i){
+                
+                ans=min(ans,i-j+1);
+                sum-=nums[j];
+                j++;
+                
+            }
+            
+            
+            
         }
         
-        if(nums[n-1]<target)return 0;
+        return ans==INT_MAX?0:ans;
         
-        int ans=n;
         
-        for(int i=0;i<n;i++){
-            
-            if(nums[i]<target){
-                continue;
-            }
-            
-            int r=nums[i]-target;
-            
-            int index=lower_bound(begin(nums),begin(nums)+i,r)-begin(nums);
-            // cout<<index<<" "<<"index"<<"\n";
-            if(index==i){
-                ans=min(ans,i-index+1);
-            }
-            if(index!=0){
-                if(nums[index]==r){
-                    ans=min(ans,i-index);
-                }
-                else{
-                    ans=min(ans,i-index+1);
-                }
-            }
-            if(index==0){
-                
-                if(nums[index]==r){
-                  ans=min(ans,i-index);
-                }
-                else ans=min(ans,i+1);
-                
-                
-            }
-             // cout<<ans<<"\n";           
-            
-        }
-        
-        return ans;
         
     }
 };
